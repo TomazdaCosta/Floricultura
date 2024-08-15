b95662b1a1e535dd5a04abdf0a45f41c92a02412
TomazdaCosta <ftomazc7@gmail.com>
2024-08-15 10:53:50 -0300

    Criado o header fixo da página

--- floricultura/src/components/header/Header.jsx
+++ floricultura/src/components/header/Header.jsx
@@ -1,6 +1,14 @@
+import logo from '../../../public/imgs/logo.png'
+import carrinho from '../../../public/imgs/carrinho.png'
+
 export default function Header() {
   return (
-    <div>
-    </div>
+    <header>
+      <div><img src={logo} /></div>
+      <div>
+        <img src={carrinho} />
+        <span>0</span>
+      </div>
+    </header>
   )
 }
--- floricultura/src/pages/fixed-layout/Layout.jsx
+++ floricultura/src/pages/fixed-layout/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../../components/header/Header";
 
 export default function Layout() {
   return (
     <>
-      <p>Esse é o layout fixo.</p>
+      <Header />
       <Outlet />
     </>
   )
