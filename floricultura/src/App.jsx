cc6921a6bd8108733885d2fb3cb6df3115b31fb6
TomazdaCosta <ftomazc7@gmail.com>
2024-08-15 10:19:40 -0300

    Criado o arquivo de contexto do site utilizando o createContext

--- floricultura/src/App.jsx
+++ floricultura/src/App.jsx
@@ -1,7 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { ContextProvider } from "./contexts/Context";
 
 export default function App() {
 
-  return <RouterProvider router={router} />
+  return (
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
+  )
 }
