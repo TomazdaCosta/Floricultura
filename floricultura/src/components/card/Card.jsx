6277c002bf285ce3ea01316bef6718228fdf6ef9
TomazdaCosta <ftomazc7@gmail.com>
2024-08-19 07:37:28 -0300

    Criada a estilização dos cards de produtos

--- floricultura/src/components/card/Card.jsx
+++ floricultura/src/components/card/Card.jsx
@@ -1,20 +1,23 @@
 import { useContext } from "react"
 import { Context } from "../../contexts/Context"
+import styles from './Card.module.css'
 
 export default function Card({id, img, nome, preco}) {
   const { addQuantidadeCarrinho } = useContext(Context)
 
   return (
-    <div>
-      <img src={img} alt={nome} />
-      <h3>{nome}</h3>
-      <div>
+    <div className={styles.card}>
+      <div className={styles.imgTitulo}>
+        <img src={img} alt={nome} />
+        <h3>{nome}</h3>
+      </div>
+      <div className={styles.precoQuantidade}>
         <div>
           <p>10 unds</p>
-          <p>{preco}</p>
+          <p className={styles.preco}>{preco}</p>
         </div>
         <div>
-          <button onClick={() => addQuantidadeCarrinho(id, img, nome, preco)}>Adicionar</button>
+          <button className={styles.botaoAdicionar} onClick={() => addQuantidadeCarrinho(id, img, nome, preco)}>Adicionar</button>
         </div>
       </div>
     </div>
