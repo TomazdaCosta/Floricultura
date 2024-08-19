c44b0e35be0e7b571d8f453bb664625e2500900d
TomazdaCosta <ftomazc7@gmail.com>
2024-08-19 07:18:41 -0300

    Criada a estiização do footer da página

--- floricultura/src/components/footer/Footer.jsx
+++ floricultura/src/components/footer/Footer.jsx
@@ -1,14 +1,17 @@
 import logo from '../../../public/imgs/logo.png'
+import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <footer>
-      <img src={logo} />
-      <ul>
-        <li>floricultura@gmail.com</li>
-        <li>+55 (99) 99192-9394</li>
-        <li>Rua Flores Campos, Fortaleza - CE</li>
-      </ul>
+    <footer className={styles.containerBg}>
+      <div className={styles.container}>
+        <img src={logo} />
+        <ul>
+          <li>floricultura@gmail.com</li>
+          <li>+55 (99) 99192-9394</li>
+          <li>Rua Flores Campos, Fortaleza - CE</li>
+        </ul>
+      </div>
     </footer>
   )
 }
