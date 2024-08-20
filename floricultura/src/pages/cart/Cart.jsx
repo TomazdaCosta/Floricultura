f52695a6d0f551a4cf25b5c00e003f3c01b043fe
TomazdaCosta <ftomazc7@gmail.com>
2024-08-20 07:06:52 -0300

    Criada a estilização da página de carrinho do site

--- floricultura/src/pages/cart/Cart.jsx
+++ floricultura/src/pages/cart/Cart.jsx
@@ -1,32 +1,38 @@
 import { useContext } from "react"
 import { Context } from "../../contexts/Context"
 import { Link } from "react-router-dom"
+import styles from './Cart.module.css'
 
 export default function Cart() {
   const { carrinho, removeCarrinho, totalItem, zerarDados } = useContext(Context)
 
   return (
-    <div>
+    <div className={styles.container}>
+      <Link to={'/'} className={styles.voltar}>⇐voltar</Link>
       {carrinho && (
         carrinho.map(({id, img, nome, preco, aleatorio}) => {
           return (
-            <div key={id}>
-              <div>
-                <img src={img} />
-                <h3>{nome}</h3>
-                <p>10 unds</p>
-                <button onClick={() => removeCarrinho(aleatorio)}>Remover</button>
-              </div>
-              <div>
-                <p>Total</p>
-                <p>{preco}</p>
+            <div key={id} className={styles.containerCard}>
+              <div className={styles.card}>
+                <div className={styles.cardDiv}>
+                  <img src={img} />
+                  <div>
+                    <h3>{nome}</h3>
+                    <p>10 unds</p>
+                    <button onClick={() => removeCarrinho(aleatorio)}>Remover</button>
+                  </div>
+                </div>
+                <div className={styles.preco}>
+                  <span>Total</span>
+                  <p>{preco}</p>
+                </div>
               </div>
             </div>
           )
         })
       )}
-      <div>
-        <p>Total</p>
+      <div className={styles.precoTotal}>
+        <span>Total</span>
         <p>{totalItem}</p>
         <Link to='/finalizado'>
           <button onClick={() => zerarDados()}>Finalizar compra</button>
