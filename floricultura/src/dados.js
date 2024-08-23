d5d7fdca773a6953772411bcdaeb365db6a245a4
TomazdaCosta <ftomazc7@gmail.com>
2024-08-23 07:42:22 -0300

    Alterações nas imagens

--- floricultura/src/dados.js
+++ floricultura/src/dados.js
@@ -3,49 +3,49 @@ const produtos = [
     id: 'tulipa',
     nome:  'Tulipa',
     preco: 39.99,
-    img: '../public/imgs/tulipa.png'
+    img: './imgs/tulipa.png'
   },
   {
     id: 'crisântemo',
     nome:  'Crisântemo',
     preco: 59.99,
-    img: '../public/imgs/crisantemo.png'
+    img: './imgs/crisantemo.png'
   },
   {
     id: 'orquídea',
     nome:  'Orquídea',
     preco: 34.99,
-    img: '../public/imgs/orquidea.png'
+    img: './imgs/orquidea.png'
   },
   {
     id: 'azaleia',
     nome:  'Azaleia',
     preco: 64.99,
-    img: '../public/imgs/azaleia.png'
+    img: './imgs/azaleia.png'
   },
   {
     id: 'rosa',
     nome:  'Rosa',
     preco: 24.99,
-    img: '../public/imgs/rosa.png'
+    img: './imgs/rosa.png'
   },
   {
     id: 'impatiens',
     nome:  'Impatiens',
     preco: 69.99,
-    img: '../public/imgs/impatiens.png'
+    img: './imgs/impatiens.png'
   },
   {
     id: 'jasmin',
     nome:  'Jasmin',
     preco: 39.99,
-    img: '../public/imgs/jasmin.png'
+    img: './imgs/jasmin.png'
   },
   {
     id: 'kalanchoe',
     nome:  'Kalanchoe',
     preco: 49.99,
-    img: '../public/imgs/kalanchoe.png'
+    img: './imgs/kalanchoe.png'
   }
 ]
 
