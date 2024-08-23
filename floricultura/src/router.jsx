d73a5786fb4fab6f701804f26af3b526369ecd49
TomazdaCosta <ftomazc7@gmail.com>
2024-08-23 07:26:13 -0300

    Teste

--- floricultura/src/router.jsx
+++ floricultura/src/router.jsx
@@ -4,25 +4,30 @@ import Products from './pages/products/Products'
 import Cart from './pages/cart/Cart'
 import OrderFinish from './pages/pedido-finalizado/OrderFinish'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Products />
+        },
+        {
+          path: 'carrinho',
+          element: <Cart />
+        },
+        {
+          path: '/finalizado',
+          element: <OrderFinish />
+        }
+      ]
+    }
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Products />
-      },
-      {
-        path: 'carrinho',
-        element: <Cart />
-      },
-      {
-        path: '/finalizado',
-        element: <OrderFinish />
-      }
-    ]
+    basename: '/Floricultura'
   }
-])
+)
 
 export default router
